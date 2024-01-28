909c081cc1ffa0efa8915944e0a1c17f09e072ab
Phinaow <phinaow@gmail.com>
2024-01-28 17:30:08 +0100

    Sequences V1

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from math import sqrt
 image = cv2.imread("sequence_02/frames/0001.bmp")'''
 
 # Charger la vidéo
-video_path = "sequence_02/seq2.mkv"
+video_path = "sequence_01/seq1.mkv"
 cap = cv2.VideoCapture(video_path)
 
 # Vérifier si la vidéo est ouverte correctement
@@ -35,7 +35,7 @@ hsv[:, :, 2] = np.zeros([height, width])
 
 for lines in range(height) :
     for columns in range(width) :
-        if(12< hsv[lines, columns, 0] and hsv[lines, columns, 0] < 40) :
+        if(12 < hsv[lines, columns, 0] and hsv[lines, columns, 0] < 40) :
             hsv[lines, columns, :] = 255
         else :
             hsv[lines, columns, :] = 0
@@ -51,17 +51,17 @@ hsv = cv2.morphologyEx(hsv, cv2.MORPH_CLOSE, kernel)
 
 hsv = sobel(hsv)
 
-R = 500
+R = 300
 
 vote = np.zeros((int(round(height, -1)/10), int(round(width, -1)/10), R))
 
-for lines in range(0, round(height, -1), 10) :
-    for columns in range(0, round(width, -1), 10) :
+for lines in range(0, round(height), 5) :
+    for columns in range(0, round(width), 5) :
         if hsv[lines, columns, 0] != 0 :
             for x0 in range(0, round(width, -1), 10) :
                 for y0 in range(0, round(height, -1), 10) :
                     r = round(sqrt((columns - x0)**2 + (lines - y0)**2))
-                    if r < 500 :
+                    if r < R :
                         vote[int(y0/10), int(x0/10), r-1] = vote[int(y0/10), int(x0/10), r-1] + 1
 
 
@@ -85,38 +85,70 @@ except BreakLoop:
 while True:
 
     # Showing images
-    cv2.circle(image, (10*x, 10*y), r, (255, 0, 0), 2)
-    cv2.imshow("image", image)
+    cv2.circle(hsv, (10*x, 10*y), r, (255, 0, 0), 2)
+    cv2.imshow("image", hsv)
 
     # Lire une nouvelle image
     ret, image = cap.read()
-    
+
     # Vérifier si la lecture de l'image a réussi
     if not ret:
         print("Fin de la vidéo.")
         break
 
     #Define the new area to analyze
-    if(x - 300 > 0) :
-        bornx_inf = x - 300
+    if(10*x - R > 0) :
+        bornx_inf = 10*x - R
     else :
         bornx_inf = 0
 
-    if(x + 300 < width) :
-        bornx_supp = x + 300
+    if(10*x + R < width) :
+        bornx_supp = 10*x + R
     else :
         bornx_supp = width
 
-    if(y - 300 > 0) :
-        borny_inf = y - 300
+    if(10*y - R > 0) :
+        borny_inf = 10*y - R
     else :
         borny_inf = 0
 
-    if(y + 300 < height) :
-        borny_supp = y + 300
+    if(10*y + R < height) :
+        borny_supp = 10*y + R
     else :
         borny_supp = height
 
+    #-----------------------------------#
+
+    if(10*x - 20 > 0) :
+        centerx_inf = 10*x - 20
+    else :
+        centerx_inf = 0
+
+    if(10*x + 20 < width) :
+        centerx_supp = 10*x + 20
+    else :
+        centerx_supp = width
+
+    if(10*y - 20 > 0) :
+        centery_inf = 10*y - 20
+    else :
+        centery_inf = 0
+
+    if(10*y + 20 < height) :
+        centery_supp = 10*y + 20
+    else :
+        centery_supp = height
+
+    if r - 20 > 0 :
+        ray_inf = r - 20
+    else :
+        ray_inf = r 
+    if r + 20 < R :
+        ray_supp = r + 20
+    else :
+        ray_supp = r
+
+    
     # Converting BGR to HSV
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
@@ -128,47 +160,43 @@ while True:
     hsv[:, :, 2] = np.zeros([height, width])
 
 
-
+    
     for lines in range(borny_inf, borny_supp) :
         for columns in range(bornx_inf, bornx_supp) :
-            if(12< hsv[lines, columns, 0] and hsv[lines, columns, 0] < 40) :
+            if(15 < hsv[lines, columns, 0] and hsv[lines, columns, 0] < 30) :
                 hsv[lines, columns, :] = 255
             else :
                 hsv[lines, columns, :] = 0
 
 
-
+    
     # Apply an opening operation
-    kernel = np.ones((20, 20), np.uint8)
+    kernel = np.ones((10, 10), np.uint8)
     hsv = cv2.morphologyEx(hsv, cv2.MORPH_OPEN, kernel)
 
     # Apply a closing operation
     #kernel = np.ones((10, 10), np.uint8)
     hsv = cv2.morphologyEx(hsv, cv2.MORPH_CLOSE, kernel)
 
-    #hsv = sobel(hsv)
+    hsv = sobel(hsv)
 
     vote = np.zeros((int(round(height, -1)/10), int(round(width, -1)/10), R))
 
-
-    for lines in range(borny_inf, borny_supp) :
-        for columns in range(bornx_inf, bornx_supp) :
+    ray = 0
+    for lines in range(borny_inf, borny_supp, 5) :
+        for columns in range(bornx_inf, bornx_supp, 5) :
             if hsv[lines, columns, 0] != 0 :
-                for x0 in range(bornx_inf, bornx_supp) :
-                    for y0 in range(borny_inf, borny_supp) :
-                        r = round(sqrt((columns - x0)**2 + (lines - y0)**2))
-                        if r < 500 :
-                            vote[int(y0/10), int(x0/10), r-1] = vote[int(y0/10), int(x0/10), r-1] + 1
-
-
-    '''class BreakLoop(Exception):
-        pass'''
+                for x0 in range(centerx_inf, centerx_supp) :
+                    for y0 in range(centery_inf, centery_supp) :
+                        ray = round(sqrt((columns - x0)**2 + (lines - y0)**2))
+                        if ray_inf < ray < ray_supp :
+                            vote[int(y0/10), int(x0/10), ray-1] = vote[int(y0/10), int(x0/10), ray-1] + 1
 
     value = np.max(vote)
     try:
-        for y in range(len(vote[:, 0, 0])) :
-            for x in range(len(vote[0, :, 0])) :
-                for r in range(len(vote[0, 0, :])) :
+        for y in range(int(round(centery_inf, -1)/10), int(round(centery_supp, -1)/10)) :
+            for x in range(int(round(centerx_inf, -1)/10), int(round(centerx_supp, -1)/10)) :
+                for r in range(ray_inf, ray_supp) :
                     if vote[y, x, r] == value :
                         raise BreakLoop
 
